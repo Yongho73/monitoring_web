55ceda5ff80472ea9837d29636aa200fbe44e2a7
naaaaaaaaaarin <kal_1991@naver.com>
2022-10-26 11:40:27 +0900

    error css 수정

--- src/home/error/404.js
+++ src/home/error/404.js
@@ -15,7 +15,7 @@ export default function Page404() {
                     <li><img src={error_four} height="100%" alt="three" title="three" /></li>
                 </ul>
                 <div>Not Found</div>
-				<p>페이지가 존재하지 않거나, 사용할 수 없는 페이지입니다.
+				<p>페이지가 존재하지 않거나, 사용할 수 없는 페이지입니다.<br />
 					입력하신 주소가 정확한지 다시 한 번 확인해주세요.
 				</p>
             </div>
