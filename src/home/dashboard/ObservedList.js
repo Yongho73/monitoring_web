84792ae7942208f6369ee1b9f137718b6e15dfe2
JinMin KIm <bizmdev@gmail.com>
2022-10-27 10:36:42 +0900

    오류 수정

--- src/home/dashboard/ObservedList.js
+++ src/home/dashboard/ObservedList.js
@@ -1,5 +1,5 @@
 import React , {useEffect , useState} from 'react'
-import getMonitoringList from '../../crud/monitoring.crud'
+import {getMonitoringList} from '../../crud/monitoring.crud'
 import CommonTablePaging from '../common/CommonTablePaging'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { regular } from '@fortawesome/fontawesome-svg-core/import.macro'
