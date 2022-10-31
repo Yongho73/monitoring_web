3d752d17271091714845dd4fad0e458bc435bf07
minjae <lmj@bizmarvel.co.kr>
2022-10-31 11:11:00 +0900

    dummy

--- src/home/util/util.js
+++ src/home/util/util.js
@@ -4,5 +4,6 @@ export function toNumber(val) {
         val = val.replaceAll(',', ''); // 잠시 콤마를 때주고
         val = val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','); 
     }
+    
     return val;
 }
