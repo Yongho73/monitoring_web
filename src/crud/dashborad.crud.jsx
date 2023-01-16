57f4d21c8024bb170314cc7e411714a10a1bd437
yongho <pyh@bizmarvel.co.kr>
2023-01-16 15:26:47 +0900

    운영반영 mysql 버전 변경

--- src/crud/dashborad.crud.jsx
+++ src/crud/dashborad.crud.jsx
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const GET_DEVICE_LIST_URL = '/api/dashboard/getDeviceList'
 const GET_DEVICE_LIST_EXCEL_URL = '/api/dashboard/getDeviceExcelList'
-const GET_DEVICE_DETAIL_URL = '/api/mongo/dashboard/getDeviceDetail'
-const GET_DEVICE_DETAIL_EXCEL_URL = '/api/mongo/dashboard/getDeviceDetailExcel'
+const GET_DEVICE_DETAIL_URL = '/api/dashboard/getDeviceDetail'
+const GET_DEVICE_DETAIL_EXCEL_URL = '/api/dashboard/getDeviceDetailExcel'
 
 
 export function getDeviceList(param) {
